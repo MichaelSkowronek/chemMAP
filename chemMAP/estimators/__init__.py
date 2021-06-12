f5601df1a18ff36b14eaa69e107eeb29b312f410
Michael Skowronek <michael.skowronek.91@gmail.com>
2021-06-12 19:44:44 +0200

    Added new evaluation function to __init__. Created evaluate_estimators.py just for estimating compounds.

--- chemMAP/estimators/__init__.py
+++ chemMAP/estimators/__init__.py
@@ -1,4 +1,5 @@
 from chemMAP.estimators.NaiveClassEstimator import NaiveClassEstimator
 from chemMAP.estimators.GenericEstimator import GenericEstimator
+from chemMAP.estimators.DecisionTree import DecisionTreeOnAtoms
 
-__all__ = [GenericEstimator, NaiveClassEstimator]
+__all__ = [GenericEstimator, NaiveClassEstimator, DecisionTreeOnAtoms]
