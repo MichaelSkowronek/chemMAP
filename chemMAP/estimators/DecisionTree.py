eede5f5c895adea7390576a6f4f5ac091669af99
Michael Skowronek <michael.skowronek.91@gmail.com>
2021-06-14 21:34:25 +0200

    Wrote Bond Features, added it to the estimator. Results are really good.

--- chemMAP/estimators/DecisionTree.py
+++ chemMAP/estimators/DecisionTree.py
@@ -1,6 +1,9 @@
+import pandas as pd
+
 from chemMAP.estimators.GenericEstimator import GenericEstimator
 from sklearn.tree import DecisionTreeClassifier
 from chemMAP.transformers.CompoundFeatures import AllAtomFeatures
+from chemMAP.transformers.CompoundFeatures import BondFeatures
 
 
 class DecisionTreeOnAtoms(GenericEstimator):
@@ -9,13 +12,18 @@ class DecisionTreeOnAtoms(GenericEstimator):
         super().__init__(ontology=ontology)
         self.tree = DecisionTreeClassifier()
         self.atomTrans = AllAtomFeatures(ontology)
+        self.bondTrans = BondFeatures(ontology)
 
     def fit(self, X, y):
-        X_features = self.atomTrans.transform(X)
-        self.tree.fit(X_features, y)
+        Atom_features = self.atomTrans.transform(X)
+        Bond_features = self.bondTrans.transform(X)
+        All_features = pd.concat((Atom_features, Bond_features), axis=1)
+        self.tree.fit(All_features, y)
 
     def predict(self, X):
-        X_features = self.atomTrans.transform(X)
-        y_pred = self.tree.predict(X_features)
+        Atom_features = self.atomTrans.transform(X)
+        Bond_features = self.bondTrans.transform(X)
+        All_features = pd.concat((Atom_features, Bond_features), axis=1)
+        y_pred = self.tree.predict(All_features)
         return y_pred
 
