4751a8a6e39dccbf35486e0772735f2d2251b1e2
Michael Skowronek <michael.skowronek.91@gmail.com>
2021-07-12 11:47:36 +0200

    Added .pcl files. Removed pickle speedup from LearningProblemParser.

--- chemMAP/LearningProblemParser.py
+++ chemMAP/LearningProblemParser.py
@@ -11,14 +11,16 @@ def get_learning_problems(source="data/kg-mini-project-train_v2.ttl"):
     (class URIRef) which are the classified individuals in the  LP ontology. The 'labels' gives the corresponding
     labels for the examples (0 for excluded, 1 for included).
 
-    The default source can be altered.
+    The default source can be altered."""
 
-    Optimized for successive loads on the same file."""
+    # Do we want to store the parsed LPs in a pickle file to speed up successive loads of the same file?
+    store_LPs_in_Pickle = False
 
-    # Have we loaded this file previously and stored it in a pickle file already? Then just load the pickle file.
-    pickled_file = source.split(".")[0]+".pcl"
-    if os.path.exists(pickled_file):
-        return pickle.load(open(pickled_file, "rb"))
+    if store_LPs_in_Pickle:
+        # Have we loaded this file previously and stored it in a pickle file already? Then just load the pickle file.
+        pickled_file = source.split(".")[0]+".pcl"
+        if os.path.exists(pickled_file):
+            return pickle.load(open(pickled_file, "rb"))
 
     # Else parse the file.
     problems_graph = Graph().parse(str(source), format="ttl")
@@ -62,6 +64,8 @@ def get_learning_problems(source="data/kg-mini-project-train_v2.ttl"):
         labels.extend(len(neg_examples) * [False])
         lp_list.append(dict(name=lp, examples=examples, labels=labels))
 
-    # Store the data structure in a pickle file to avoid successive parsing.
-    pickle.dump(lp_list, open(pickled_file, "wb"))
+    if store_LPs_in_Pickle:
+        # Store the data structure in a pickle file to avoid successive parsing.
+        pickle.dump(lp_list, open(pickled_file, "wb"))
+
     return lp_list
